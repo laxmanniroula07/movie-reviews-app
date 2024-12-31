4b4667a9ce650da5fa70d0f486c1b9681ef3dfc6
laxmanniroula07 <laxmanniroula314@gmail.com>
2024-12-31 08:04:45 +0545

    upload file

---
+++ movies-review/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.scss';
+
+function App() {
+  return (
+      
+    <h1> app is the main rtis fjdfbbbjnvjhjvcn hvcbgcvhvjhxjhv</h1>
+    
+        
+  
+  );
+}
+
+export default App;
